06411db25670027c40d37846cbf0710228572245
Piyush Pradhan <30986281+piyushpradhan22@users.noreply.github.com>
2026-05-05 23:12:06 +0530

    Update keep_it_active.py

--- keep_it_active.py
+++ keep_it_active.py
@@ -6,10 +6,15 @@ try:
 except Exception as e:
     print(f"Stremio request failed: {e}")
 try:
-    response = requests.get("https://reinkingkrauszwvrx10571733757044-qb.hf.space/")
+    response = requests.get("https://guzowskilynottmvvy49011733757114-qb.hf.space/")
     if 'content="qBittorrent WebUI' in response.text:
         print("QB is running Well")
     else:
         print("QB is not running Well", response.text)
 except Exception as e:
     print(f"QB request failed: {e}")
+try:
+    requests.get("https://guzowskilynottmvvy49011733757114-ptn.hf.space").json()
+    print("PTN is running Well")
+except Exception as e:
+    print(f"PTN request failed: {e}")
